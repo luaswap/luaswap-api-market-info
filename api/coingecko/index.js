1bf1f1ea4efd60f24c2e5dc0e8cc79aecfa5a719
PhucNH <phucnh.dev@gmail.com>
2020-11-04 10:15:14 +0700

    Fix rate limit

--- api/coingecko/index.js
+++ api/coingecko/index.js
@@ -1,9 +1,48 @@
 const express = require('express')
+const _ = require('lodash')
 const router = express.Router()
+const rateLimit = require("express-rate-limit")
+const limiterMarket = rateLimit({
+    windowMs: 1 * 60 * 1000, // 1 minutes
+    max: 10, // limit each IP to 10 requests per windowMs
+    handler: function (req, res, next) {
+        res.status(429).json({
+            status: 429,
+            message: 'Too many requests, please try again later.'
+        })
+    }
+})
 
+const limiterEachPair = rateLimit({
+    windowMs: 1 * 60 * 1000, // 1 minutes
+    max: 10, // limit each ticker_id to 10 requests per windowMs
+            // if don't have ticker_id param in request, limit by ip
+    keyGenerator: function (req /*, res*/) {
+        return (req.ticker_id && _.trim(req.ticker_id)) ? _.trim(req.ticker_id) : req.ip
+    },
+    handler: function (req, res, next) {
+        res.status(429).json({
+            status: 429,
+            message: 'Too many requests, please try again later.'
+        })
+    }
+})
 
-router.use('/pairs', require('./pair'))
-router.use('/tickers', require('./ticker'))
-router.use('/orderbook', require('./orderbook'))
-router.use('/historical_trades', require('./historicalTrade'))
+const limiterEachPairByIp = rateLimit({
+    windowMs: 1 * 60 * 1000, // 1 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+    handler: function (req, res, next) {
+        res.status(429).json({
+            status: 429,
+            message: 'Too many requests, please try again later.'
+        })
+    }
+})
+
+router.use('/pairs', limiterMarket, require('./pair'))
+router.use('/tickers', limiterMarket, require('./ticker'))
+router.use('/orderbook', limiterEachPair, require('./orderbook'))
+router.use('/orderbook', limiterEachPairByIp)
+router.use('/historical_trades', limiterEachPair, require('./historicalTrade'))
+router.use('/historical_trades', limiterEachPairByIp)
 module.exports = router
--- index.js
+++ index.js
@@ -3,7 +3,6 @@ require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
 const compression = require('compression')
-const rateLimit = require("express-rate-limit")
 const config = require('config')
 global.ASYNC = fn => (req, res, next) => Promise.resolve(fn(req, res, next)).catch(next)
 const port = config.get("port") || 8889
@@ -18,17 +17,6 @@ app.use(bodyParser.json())
 
 // Begin setting api
 app.enable('trust proxy')
-const limiter = rateLimit({
-  windowMs: 1 * 60 * 1000, // 1 minutes
-  max: 10, // limit each IP to 10 requests per windowMs
-  handler: function (req, res, next) {
-    res.status(429).json({
-      status: 429,
-      message: 'Too many requests, please try again later.'
-    })
-  }
-})
-app.use('/api/', limiter)
 app.use('/api/coingecko', require('./api/coingecko'))
 app.use('/api', require('./middlewares/error'))
 // End setting api
