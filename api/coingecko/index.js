f9ad297ae64824505223f6609107348ef979c951
PhucNH <phucnh.dev@gmail.com>
2020-11-04 16:52:39 +0700

    Use etherproject/address to get address checksum

--- api/coingecko/index.js
+++ api/coingecko/index.js
@@ -18,7 +18,7 @@ const limiterEachPair = rateLimit({
     max: 10, // limit each ticker_id to 10 requests per windowMs
             // if don't have ticker_id param in request, limit by ip
     keyGenerator: function (req /*, res*/) {
-        return (req.ticker_id && _.trim(req.ticker_id)) ? _.trim(req.ticker_id) : req.ip
+        return (req.query.ticker_id && _.trim(req.query.ticker_id)) ? _.trim(req.query.ticker_id) : req.ip
     },
     handler: function (req, res, next) {
         res.status(429).json({
